628ed1cbd2cc231fb32545cfc0e5c85035dd5960
keigo <b1951154@planet.kanazawa-it.ac.jp>
2021-11-05 00:04:22 +0900

    ステージの自動生成追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController controller;
+
+    private Vector3 movedir = Vector3.zero;
+    // Playerの移動値を入れる変数
+    public float speedZ;
+    public float acceleratorZ;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movedir.z = Mathf.Clamp(movedir.z + (acceleratorZ * Time.deltaTime), 0, speedZ);
+        Vector3 globaldir = transform.TransformDirection(movedir);
+        controller.Move(globaldir * Time.deltaTime);
+
+    }
+}
